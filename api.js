d900da50aaefdfb7c151cf006b40d8466e776372
Harish Mamidi <harish.mamidi.kumar@gmail.com>
2020-06-11 23:01:19 +0530

    Date added

--- api.js
+++ api.js
@@ -1,13 +1,14 @@
-const covidStatsCases = document.querySelector('.covid-stats-cases')
-const covidStatsDeath = document.querySelector('.covid-stats-death')
-const covidStatsRecovered = document.querySelector('.covid-stats-recovered')
-
-const api = fetch('https://api.covid19api.com/summary')
+const covidStatsCases = document.querySelector('.covid-stats-cases');
+const covidStatsDeath = document.querySelector('.covid-stats-death');
+const covidStatsRecovered = document.querySelector('.covid-stats-recovered');
+const covidStatsTime = document.querySelector('.covid-update-time');
+const api = fetch('https://api.covid19api.com/summary');
 
 api.then((data) => {
-    data.json().then((obj) => {
-        covidStatsCases.innerHTML = obj.Global.TotalConfirmed
-        covidStatsDeath.innerHTML = obj.Global.TotalDeaths
-        covidStatsRecovered.innerHTML = obj.Global.TotalRecovered
-    })
-})
+  data.json().then((obj) => {
+    covidStatsCases.innerHTML = obj.Global.TotalConfirmed;
+    covidStatsDeath.innerHTML = obj.Global.TotalDeaths;
+    covidStatsRecovered.innerHTML = obj.Global.TotalRecovered;
+    covidStatsTime.innerHTML = new Date(obj.Date);
+  });
+});
